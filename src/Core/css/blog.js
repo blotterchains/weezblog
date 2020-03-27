8550659b911f4902c07fddbd1094dd0b05b98e96
wizif <rezagina68@gmail.com>
2020-03-28 03:30:11 +0430

    this file added instead of home.js

--- src/Core/css/blog.js
+++ src/Core/css/blog.js
@@ -1,33 +1,87 @@
+export const whiteSection={
+    backgroundColor: "rgba(255, 255, 255,.6)",
+                    color:"black",
+                    padding:"20px",
+                    borderRadius:"5px",
+                    marginLeft:"1em",
+                    marginRight:"1em",
+                    height:"100%",
+                    textAlign:"left"
+}
+export const greenSection={
+    backgroundColor: "rgb(51, 255, 119,.6)",
+                    color:"black",
+                    padding:"20px",
+                    borderRadius:"5px",
+                    marginLeft:"1em",
+                    marginRight:"1em",
+                    textAlign:"left"
+
+}
+export const redSection={
+    backgroundColor: "rgba(255, 255, 255,.6)",
+                    color:"black",
+                    padding:"20px",
+                    borderRadius:"5px",
+                    marginLeft:"1em",
+                    marginRight:"1em",
+                    textAlign:"left"
+
+}
+export const yellow={
+    backgroundColor: "rgba(255, 255, 255,.6)",
+                    color:"black",
+                    padding:"20px",
+                    borderRadius:"5px",
+                    marginLeft:"1em",
+                    marginRight:"1em",
+                    textAlign:"left"
+
+}
 export const sidebar = {
-    height:  "23vh",
+    height:  "70vh",
     width: "20vw",
     overflow: "auto",
     borderRadius: "6px",
     margin: "4px",
     color: "#d7d6d5",
-    textAlign:"center",
     paddingTop:"2vh",
-    backgroundImage: "linear-gradient(rgba(243, 0, 64,0.8), rgba(23, 70, 83,0.8))"
+    backgroundImage: "linear-gradient(rgb(166, 166, 166,0.8), rgb(255, 204, 204))"
 }
-export const blogbdscroll={
-    fontSize:"3vmin",
+export const blogbdscrollDesk={
     overflowY: "scroll",
     overflowX: "hidden",
     scrollbarColor: "rgba(243, 0, 64) rgba(23, 70, 83)",
     height:"87vh",
-    paddingRight:"10px",
+    borderLeft:"2px solid rgba(0,0,0,.5)",
 }
-export const blogbd = {
+export const blogbdscrollMob={
+    overflowY: "scroll",
+    overflowX: "hidden",
+    height:"87vh",
+}
+export const blogbdDesk = {
     top:0,
-    width: "75vw",
-    padding:"1em",
+    paddingTop:"2vh",
+    paddingBottom:"2vh",
+    width: "65vw",
     borderRadius: "6px",
-    margin: "10px",
+    margin: "0px",
     color: "#d7d6d5",
-    backgroundImage: "linear-gradient(rgba(243, 0, 64,0.8), rgba(23, 70, 83,0.8))"
+    backgroundImage: "linear-gradient(rgb(166, 166, 166,0.8), rgb(255, 204, 204))"
+}
+export const blogbdMob = {
+    top:0,
+    width: "100%",
+    paddingTop:"2vh",
+    paddingBottom:"2vh",
+    borderRadius: "6px",
+   
+    color: "#d7d6d5",
+    backgroundImage: "linear-gradient(rgb(166, 166, 166,0.8), rgb(255, 204, 204))"
 }
 
-export const BODYresponsive={
+export const blogBody={
     
     
 }
