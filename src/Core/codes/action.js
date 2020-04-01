afd977322474c4bb6b465b3c419cdf19038c23b2
wizif <rezagina68@gmail.com>
2020-04-02 01:39:14 +0430

    MakeBodyWithRoutes component changed from function to class

--- src/Core/codes/action.js
+++ src/Core/codes/action.js
@@ -1,13 +1,20 @@
 import { Route } from "react-router-dom"
 import React from 'react';
-export function makebodyWithRoutes(route){
+export class MakebodyWithRoutes extends React.Component{
+    constructor(props){
+        super(props);
+        this.route=this.props.route;
+    }
+    render(){
     return(
-        route.map((index,key)=>{
+        this.route.map((index,key)=>{
             return(
                 <Route exact key={key} path={index.path} render={() =>index.render}/>
             )
-        })
+        }
+        )
     )
+    }
 }
 export function reactRouteLink(history,route){
         history.push(route);
